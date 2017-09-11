03bffa7ed5263a6184c523c81ded778215aa9338
jphager2 <jphager2@gmail.com>
2017-09-11 14:45:52 +0200

    record source

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./lib/goodreads');
 require('./lib/modern_library');
 require('./lib/telegraph');
-require('./lib/the_greatest_book');
+require('./lib/the_greatest_books');
 require('./lib/the_guardian');
